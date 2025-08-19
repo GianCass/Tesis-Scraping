c66d224cfefe5f1194795d667eddd8a5ef19c08a
sage <antoshkakorol301@gmail.com>
2025-08-18 21:21:09 -0500

    apache functional dockeroperator

--- Dockerfile
+++ Dockerfile
@@ -1,38 +1,38 @@
 FROM apache/airflow:2.8.2-python3.9
 
-USER root
-RUN apt-get update && apt-get install -y \
-    build-essential \
-    python3-dev \
-    python3-setuptools \
-    python3-wheel \
-    libhdf5-dev \
-    libyaml-dev \
-    libhdf5-serial-dev \
-    libhdf5-103 \
-    pkg-config \
-    zlib1g-dev \
-    libssl-dev \
-    libbz2-dev \
-    liblzma-dev \
-    && rm -rf /var/lib/apt/lists/*
+# USER root
+# RUN apt-get update && apt-get install -y \
+#     build-essential \
+#     python3-dev \
+#     python3-setuptools \
+#     python3-wheel \
+#     libhdf5-dev \
+#     libyaml-dev \
+#     libhdf5-serial-dev \
+#     libhdf5-103 \
+#     pkg-config \
+#     zlib1g-dev \
+#     libssl-dev \
+#     libbz2-dev \
+#     liblzma-dev \
+#     && rm -rf /var/lib/apt/lists/*
 
-# Variables de entorno para que h5py detecte HDF5
-ENV HDF5_DIR=/usr/lib/x86_64-linux-gnu/hdf5/serial
-ENV CC=gcc
+# # Variables de entorno para que h5py detecte HDF5
+# ENV HDF5_DIR=/usr/lib/x86_64-linux-gnu/hdf5/serial
+# ENV CC=gcc
 
-USER airflow
+# USER airflow
 
-# Actualizar herramientas de compilación
-RUN pip install --upgrade pip setuptools wheel cython
-RUN pip install --no-cache-dir "h5py==3.10.0"
+# # Actualizar herramientas de compilación
+# RUN pip install --upgrade pip setuptools wheel cython
+# RUN pip install --no-cache-dir "h5py==3.10.0"
 
-# Instalar h5py primero (para aislar el error) y luego el resto
-# COPY requirements.txt /requirements.txt
-COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt \
-    --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.8.2/constraints-3.9.txt"
+# # Instalar h5py primero (para aislar el error) y luego el resto
+# # COPY requirements.txt /requirements.txt
+# COPY requirements.txt .
+# RUN pip install --no-cache-dir -r requirements.txt \
+#     --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.8.2/constraints-3.9.txt"
 
-COPY requirements2.txt .
-RUN pip install --no-cache-dir -r requirements2.txt \
-    --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.8.2/constraints-3.9.txt"
+# COPY requirements2.txt .
+# RUN pip install --no-cache-dir -r requirements2.txt \
+#     --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.8.2/constraints-3.9.txt"
--- dags/scraping_dags.py
+++ dags/scraping_dags.py
@@ -1,29 +1,73 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
+from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.models.baseoperator import chain
+from docker.types import Mount
 
 # docker compose build --no-cache
 # docker compose up -d
 
+# Si Airflow corre EN Docker y quieres usar su red:
+network_mode="airflow_net"
+# Si Airflow corre FUERA de Docker (instalación nativa), omite network_mode.
+
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2025, 8, 1),
     'retries': 1,
+    # 'retries': 5,
     'retry_delay': timedelta(minutes=5),
 }
 
 with DAG(
     'scraping_main_run',
+    start_date = datetime(2025, 8, 18),
     default_args=default_args,
     schedule_interval='@weekly',
-    catchup=False
+    catchup=False,
+    tags=["scrapy", "seleniumbase", "captcha_solver"],
 ) as dag:
 
-    run_scraping = BashOperator(
-        task_id='run_scraping_script',
-        # bash_command='cd /opt/airflow/tesis-scraping && source venv/bin/activate && python3 extraccion/main.py'
-        # bash_command='cd /opt/airflow/tesis-scraping && ls && venv/bin/python3 extraccion/main.py'
-        bash_command='cd /opt/airflow/tesis-scraping && source venv/bin/activate && python extraccion/main.py'
+    # run_scraping = BashOperator(
+    #     task_id='run_scraping_script',
+    #     bash_command='cd /opt/airflow/tesis-scraping && source venv/bin/activate && python extraccion/main.py'
 
-
+    # )
+    scrape_prods_vars = DockerOperator(
+        task_id="scrape_prods_vars",
+        image="spi_scraper:latest",
+        api_version="auto",
+        auto_remove=True,
+        mem_limit="4g",        # más RAM para Chrome
+        shm_size="2g",         # /dev/shm grande: evita crashes de Chrome
+        command=["python", "extraccion/main.py"],
+        docker_url="unix://var/run/docker.sock",
+        network_mode="airflow_net",  # si usas la red compartida
+        environment={
+            "FLARESOLVERR_URL": "http://flaresolverr:8191",
+            "MONGO_URI": "mongodb://host.docker.internal:27017/bodies_scraping",
+            "SB_HEADLESS": "1",
+            "SB_ARGS": "--no-sandbox --disable-dev-shm-usage --disable-gpu --window-size=1920,1080",
+        },
+        mount_tmp_dir=False,
+        extra_hosts={"host.docker.internal": "host-gateway"},
+        # monta si quieres que los datos salgan al host
+        mounts=[
+            Mount(
+                source="/Users/sage/Documents/spiView/Tesis-Scraping/dataset",
+                target="/app/dataset",
+                type="bind",
+            ),
+            Mount(
+                source="/Users/sage/Documents/spiView/Tesis-Scraping/extraccion/dataset",
+                target="/app/extraccion/dataset",
+                type="bind",
+            ),
+            # Optional:
+            # Mount(
+            #     source="/Users/sage/Documents/spiView/Tesis-Scraping/data",
+            #     target="/app/data",
+            #     type="bind",
+            # ),
+        ],
     )
